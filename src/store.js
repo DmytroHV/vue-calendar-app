3c6c8f5ae932660a690a8c61cdbb98c8b91e088b
Dmytro Halimov <dmitrii.halimov@acropolium.com>
2018-07-09 18:14:42 +0300

    add delete event feature

--- src/store.js
+++ src/store.js
@@ -40,5 +40,12 @@ export const store = {
     const dayObj = this.state.seedData.find(day => day.id === dayId);
 
     return dayObj.events.find(event => event.details === eventDetails);
+  },
+  deleteEvent(dayId, eventDetails) {
+    const dayObj = this.state.seedData.find(day => day.id === dayId);
+    const eventIndexToRemove = dayObj.events.findIndex(
+      event => event.details === eventDetails
+    );
+    dayObj.events.splice(eventIndexToRemove, 1);
   }
 };
